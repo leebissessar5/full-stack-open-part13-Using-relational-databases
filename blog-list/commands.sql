7571d6c2b703ed59d88f4cfaac76c1d2d08d1666
leebissessar5 <leebissessar@gmail.com>
2023-06-22 08:05:22 -0400

    complete ex13.2

---
+++ blog-list/commands.sql
@@ -0,0 +1,12 @@
+CREATE TABLE blogs (
+  id SERIAL PRIMARY KEY,
+  author VARCHAR(255),
+  url VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  likes INTEGER DEFAULT 0
+);
+
+INSERT INTO blogs (author, url, title, likes)
+VALUES
+  ('John Doe', 'https://example.com/blog1', 'First Blog Post', 10),
+  ('Jane Smith', 'https://example.com/blog2', 'Second Blog Post', 5);
