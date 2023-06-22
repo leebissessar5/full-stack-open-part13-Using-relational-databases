08360e8569f770a1b3104cca2b33b07e641abbe5
leebissessar5 <leebissessar@gmail.com>
2023-06-22 08:27:39 -0400

    add GET and POST requests

--- blog-list/index.js
+++ blog-list/index.js
@@ -18,7 +18,6 @@ const main = async () => {
     blogs.map((blog) =>
       console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
     )
-    sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
@@ -29,4 +28,50 @@ main()
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+})
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+)
+
+app.get('/api/blogs', async (req, res) => {
+  const notes = await Blog.findAll()
+  res.json(notes)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const note = await Blog.create(req.body)
+    return res.json(note)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
 })
