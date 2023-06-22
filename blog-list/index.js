d87a4161ceb5b17c536351d2a7887e19f85329a4
leebissessar5 <leebissessar@gmail.com>
2023-06-22 08:20:34 -0400

    correction to exercise ex13.3

--- blog-list/index.js
+++ blog-list/index.js
@@ -1,15 +1,23 @@
 require('dotenv').config()
 
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, QueryTypes, DataTypes } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const express = require('express')
+const app = express()
+
+// Middleware to parse JSON data
+app.use(express.json())
 
 const main = async () => {
   try {
     const blogs = await sequelize.query('SELECT * FROM blogs', {
       type: QueryTypes.SELECT,
     })
-    console.log(blogs)
+    blogs.map((blog) =>
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    )
     sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
@@ -17,3 +25,8 @@ const main = async () => {
 }
 
 main()
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
