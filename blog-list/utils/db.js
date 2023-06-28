0ca33c096e556b53d290dda1c098df5c5ab5523b
leebissessar5 <leebissessar@gmail.com>
2023-06-28 09:55:40 -0400

    add migrations

--- blog-list/utils/db.js
+++ blog-list/utils/db.js
@@ -1,11 +1,31 @@
 require('dotenv').config()
 
 const { Sequelize, QueryTypes } = require('sequelize')
-
+const { Umzug, SequelizeStorage } = require('umzug')
 const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf)
+  const migrations = await migrator.up()
+
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  })
+}
 
 const connectToDatabase = async () => {
   try {
+    await sequelize.authenticate()
+    await runMigrations()
     const blogs = await sequelize.query('SELECT * FROM blogs', {
       type: QueryTypes.SELECT,
     })
